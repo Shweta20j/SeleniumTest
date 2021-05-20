10822cfa373cceceb92684e401f54095412d08b8
ShwetaJain <shweta.jain@oncehub.com>
2021-05-20 12:58:19 +0530

    New files

--- AutomationScript/Signup_and_SO_product_selection/Signup_and_SO_onboarding.py
+++ AutomationScript/Signup_and_SO_product_selection/Signup_and_SO_onboarding.py
@@ -13,6 +13,7 @@ class Signup_SO_Onboarding():
     #################################  launch Chrome server  #################################
 
     driver = None
+    #test
 
     def __init__(self, driver):
         self.driver = driver
